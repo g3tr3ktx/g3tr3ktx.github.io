aa9904e58474e4e56702b0853aea33e99ea3a89f
Samar Sheikh <samarsheikh001@gmail.com>
2021-02-06 00:47:31 +0530

    x

---
+++ apps/downloaderScript.js
@@ -0,0 +1,38 @@
+var imageLink;
+var PDFDownloadLink;
+
+
+// Defining async function 
+async function getapi(bookName) {
+    downloadLink.style.display = "none";
+    // Storing response 
+    const response = await fetch("https://www.googleapis.com/books/v1/volumes?q=" + bookName);
+    const myPDF = await fetch("https://sheltered-lake-50186.herokuapp.com/" + bookName);
+    console.log(myPDF);
+    var PDFData = await myPDF.json();
+
+    // Storing data in form of JSON 
+    var data = await response.json();
+    // console.log(data); 
+    if (response) {
+        imageLink = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"];
+        PDFDownloadLink = PDFData["data"][0];
+        console.log(PDFDownloadLink);
+    }
+    downloadLink.style.display = "block";
+    // show(data); 
+}
+// Calling that async function 
+// getapi(api_url); 
+
+
+async function printName() {
+    console.log(bookNameInput.value);
+    await getapi(bookNameInput.value);
+    bookCoverImage.src = imageLink;
+    downloadLink.href = PDFDownloadLink;
+}
+
+var bookNameInput = document.getElementById("bookName");
+var bookCoverImage = document.getElementById("bookCover");
+var downloadLink = document.getElementById("downloadLink");
